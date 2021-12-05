07e2fb03c8e9ee3f10ade88093cdc1085da37993
Aleksandr.Kashirin <saniok26@rambler.ru>
2021-12-05 11:53:51 +0200

    first-task

---
+++ mod-1-2.js
@@ -0,0 +1,34 @@
+// const numbers = [1, 14, 42, 69, 87, 96, 8, 9, 12, 13, 7];
+// let sum ;
+// for (const num of numbers) {
+//     if (num % 2 !== 0) {
+//         sum += num;
+//     }
+// }
+// console.log('sum', sum);
+
+// console.log(str);
+// let str = 299;
+// var str = 'console';
+
+
+//1. Используя функцию if...else,
+//напишите код, который будет спрашивать:
+//"Какое официальное название JavaScript?"
+//Если пользователь вводит "ECMAScript",
+//то показать через alert: "Верно!"
+//в противном случае отобразить:"Не знаете? ECMAScript!"
+
+
+// const userInput = prompt('Какое официальное название JavaScript ?');
+// console.log(userInput);
+// if (userInput === "ECMAScript") {
+//     alert("Верно!")    
+// } else {
+//     alert("Не знаете? ECMAScript!")
+// }
+
+// userInput === "ECMAScript"
+//     ? alert("Верно!")
+//     : alert("Не знаете? ECMAScript!");
+
