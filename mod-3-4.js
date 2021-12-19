7498300e6fc43648e0e43b980480802a23030d28
Aleksandr.Kashirin <saniok26@rambler.ru>
2021-12-19 12:17:40 +0200

    task-2

--- mod-3-4.js
+++ mod-3-4.js
@@ -1,10 +1,10 @@
 // 3 Напишите фукцио преобраения данных
-const inputData = [
-  { id: 1, name: "Item 1" },
-  { id: 2, name: "Item 2" },
-  { id: 4, name: "Item 4" },
-  { id: 7, name: "Item 7" },
-];
+// const inputData = [
+//   { id: 1, name: "Item 1" },
+//   { id: 2, name: "Item 2" },
+//   { id: 4, name: "Item 4" },
+//   { id: 7, name: "Item 7" },
+// ];
 
 // ('function should return normalized data', () => {
 //     expect(normalize(inputData)).toEqual({
@@ -17,24 +17,51 @@ const inputData = [
 //       byId: [1, 2, 4, 7],
 //     });
 
-const normalizedInput = (data) => {
-  const objData = {};
+// const normalizedInput = (data) => {
+//   const objData = {};
 
-  const idArray = [];
+//   const idArray = [];
 
-  const normData = {};
+//   const normData = {};
 
-  data.forEach((obj) => {
-    let objId = obj.id;
-    idArray.push(objId);
-    objData[objId] = obj;
-  });
+//   data.forEach((obj) => {
+//     let objId = obj.id;
+//     idArray.push(objId);
+//     objData[objId] = obj;
+//   });
 
-  normData.entities = objData;
+//   normData.entities = objData;
 
-  normData.byId = idArray;
-  console.log("normalizedInput - normData", normData);
+//   normData.byId = idArray;
+//   console.log("normalizedInput - normData", normData);
 
-  return normData;
-};
-console.log("normalizedInput - normalizedInput", normalizedInput(inputData));
+//   return normData;
+// };
+// console.log("normalizedInput - normalizedInput", normalizedInput(inputData));
+
+
+// Записать массив, const arr = [“BEST”, “the”, “foo”, “is”,  “JS” ]
+// развернуть массив,
+// вырезать foo,
+// перевести его в строку разделенную пробелами
+
+const arr = ['BEST', 'the', 'foo', 'is',  'JS'];
+
+const newStr = (arr, str) => {
+  // const arrReverse = arr.reverse();
+  // console.log(arrReverse);
+  // const deleteElement = arrReverse.splice(num - 1, 1);
+  // console.log(deleteElement);
+  // return arrReverse.join(' ')
+
+  // const resultStr = arr.reverse().slice(0, num - 1).concat(arr.slice(num)).join(' ');
+  // return resultStr;
+
+  const resultString = arr.reverse().filter(item => item !== str).join(' ');
+
+  return resultString;
+}
+console.log(newStr(arr, 'foo'));
+
+
+
